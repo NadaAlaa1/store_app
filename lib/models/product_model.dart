f594b99925e54a33000c41f663998581cb412ffc
NadaAlaa1 <nalaa839@gmail.com>
2024-12-08 21:02:55 +0200

    add all products service

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -2,6 +2,7 @@ class ProductModel {
   final int id;
   final String title, description, image;
   final double price;
+  final RatingModel rating;
 
   ProductModel({
     required this.id,
@@ -9,6 +10,7 @@ class ProductModel {
     required this.description,
     required this.image,
     required this.price,
+    required this.rating,
   });
 
   factory ProductModel.fromJson(jsonData) {
@@ -18,6 +20,24 @@ class ProductModel {
       description: jsonData['description'],
       image: jsonData['image'],
       price: jsonData['price'],
+      rating: RatingModel.fromJson(jsonData['rating'])
+    );
+  }
+}
+
+class RatingModel {
+  final double rate;
+  final int count;
+
+  RatingModel({
+    required this.rate,
+    required this.count,
+  });
+
+  factory RatingModel.fromJson(jsonData) {
+    return RatingModel(
+      rate: jsonData['rate'],
+      count: jsonData['count'],
     );
   }
 }
