69f637ae6074cc2c6870237d908c192960bddaf3
NadaAlaa1 <nalaa839@gmail.com>
2024-12-09 15:11:16 +0200

    add product service

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -1,15 +1,40 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 
-class Api{
+class Api {
   Future<dynamic> get({required String url}) async {
     http.Response response = await http.get(Uri.parse(url));
 
-    if(response.statusCode == 200){
+    if (response.statusCode == 200) {
       return jsonDecode(response.body);
     } else {
-      throw Exception('There is a problem with status code ${response.statusCode}');
+      throw Exception(
+          'There is a problem with status code ${response.statusCode}');
+    }
+  }
+
+  Future<dynamic> post(
+      {required String url,
+      @required dynamic body,
+      @required String? token}) async {
+    Map<String, String> headers = {};
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+    http.Response response = await http.post(
+      Uri.parse(url),
+      body: body,
+      headers: headers,
+    );
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = jsonDecode(response.body);
+      return data;
+    } else {
+      throw Exception(
+          'There is a problem with status code ${response.statusCode} with body ${jsonDecode(response.body)}');
     }
   }
 }
